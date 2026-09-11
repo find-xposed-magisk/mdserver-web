3b78079cd29dea482da30e39290a4356c39c86b0
dami <midoks@163.com>
2026-09-11 19:35:40 +0800

    Update install.sh

--- plugins/mysql/install.sh
+++ plugins/mysql/install.sh
@@ -6,13 +6,13 @@ export PATH
 # https://www.cnblogs.com/whiteY/p/17331882.html
 
 # cd /www/server/mdserver-web/plugins/mysql && bash install.sh install 5.5
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/mysql/index.py try_slave_sync_bugfix {}
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/mysql/index.py do_full_sync  {"db":"xxx","sign":"","begin":1}
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/mysql/index.py sync_database_repair  {"db":"xxx","sign":""}
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/mysql/index.py init_slave_status
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/mysql/index.py install_pre_inspection
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/mysql/index.py set_slave_status {"close":"change"}
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/mysql/index.py set_root_pwd {"password":"root","force":"2"}
+# cd /www/server/mdserver-web && . bin/activate && python3 plugins/mysql/index.py try_slave_sync_bugfix {}
+# cd /www/server/mdserver-web && . bin/activate && python3 plugins/mysql/index.py do_full_sync  {"db":"xxx","sign":"","begin":1}
+# cd /www/server/mdserver-web && . bin/activate && python3 plugins/mysql/index.py sync_database_repair  {"db":"xxx","sign":""}
+# cd /www/server/mdserver-web && . bin/activate && python3 plugins/mysql/index.py init_slave_status
+# cd /www/server/mdserver-web && . bin/activate && python3 plugins/mysql/index.py install_pre_inspection
+# cd /www/server/mdserver-web && . bin/activate && python3 plugins/mysql/index.py set_slave_status {"close":"change"}
+# cd /www/server/mdserver-web && . bin/activate && python3 plugins/mysql/index.py set_root_pwd {"password":"root","force":"2"}
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
 rootPath=$(dirname "$rootPath")
