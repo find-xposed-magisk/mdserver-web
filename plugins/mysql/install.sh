07a0a1be481512c108d3b85802651cb3e97fd0d2
dami <midoks@163.com>
2026-09-11 19:31:44 +0800

    Update install.sh

--- plugins/mysql/install.sh
+++ plugins/mysql/install.sh
@@ -19,7 +19,7 @@ rootPath=$(dirname "$rootPath")
 serverPath=$(dirname "$rootPath")
 
 if [ -f ${rootPath}/bin/activate ];then
-	source ${rootPath}/bin/activate
+	. ${rootPath}/bin/activate
 fi
 
 action=$1
